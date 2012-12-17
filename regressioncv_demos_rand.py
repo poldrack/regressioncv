232e461ea2b4602b2fdfac8456ada437c6a728d5
Yaroslav Halchenko <debian@onerussian.com>
2012-12-17 11:56:27 -0500

    ENH: for KFolds also estimate the means of corrcoef's across splits

--- regressioncv_demos_rand.py
+++ regressioncv_demos_rand.py
@@ -54,13 +54,18 @@ def get_sample_balcv(x,y,nfolds,pthresh=0.8):
     y=y.reshape((nsubs,1))
     
     pred=N.zeros((nsubs,1))
-    
+
+    corrs = []
     for train,test in cv:
         lr=LinearRegression()
         lr.fit(x[train,:],y[train,:])
-        pred[test]=lr.predict(x[test])
+        p = lr.predict(x[test])
+        pred[test] = p
+        corrs.append(N.corrcoef(p[:,0], y[test, 0])[0, 1])
 
-    return N.corrcoef(pred[:,0],y[:,0])[0,1]
+    # return both -- corrcoef over all prediction/estimates pairs, and
+    # mean over corrcoef within each split
+    return N.corrcoef(pred[:,0],y[:,0])[0,1], np.mean(corrs)
 
 
 # data digitized from Demos et al. paper
@@ -74,7 +79,10 @@ nruns=1000
 
 # save all the various corrs into a single dict to make saving easier
 
-corrs={'splithalf':N.zeros(nruns),'loo':N.zeros(nruns),'balcv_lo':N.zeros(nruns),'balcv_hi':N.zeros(nruns)}
+corrs=dict([(k, N.zeros(nruns)) 
+            for k in ('splithalf', 'loo',
+                      'balcv_lo', 'balcv_hi',
+                      'mbalcv_lo', 'mbalcv_hi')])
 
 for run in range(nruns):
     N.random.shuffle(x_all)
@@ -88,8 +96,9 @@ for run in range(nruns):
     corrs['loo'][run]=N.corrcoef(pred_loo[:,0],y_all[:,0])[0,1]
 
     # get results with balanced CV for high and low threshold
-    corrs['balcv_lo'][run]=get_sample_balcv(x_all,y_all,4,pthresh=0.001)
-    corrs['balcv_hi'][run]=get_sample_balcv(x_all,y_all,4,pthresh=0.9)
+    corrs['balcv_lo'][run], corrs['mbalcv_lo'][run] = get_sample_balcv(x_all,y_all,4,pthresh=0.001)
+    corrs['balcv_hi'][run], corrs['mbalcv_hi'][run] = get_sample_balcv(x_all,y_all,4,pthresh=0.9)
+
 
 
 f=open('loosim_%s_data_%d_subs.pkl'%(identifier,nsubs),'wb')
@@ -100,5 +109,25 @@ print 'Mean correlation (predicted,true):'
 for k in corrs.iterkeys():
      print k,N.mean(corrs[k])
 
-
+import pylab as pl
+print "Scatter-plotting balcv's against means"
+for k in ('balcv_lo', 'balcv_hi'):
+    corrs_, mcorrs_ = corrs[k], corrs['m'+k]
+    mk = 'm' + k
+    minmax = np.max(np.abs(np.array((corrs_, mcorrs_))))
+    fig = pl.figure(figsize=(12, 5))
+    pl.subplot(1, 3, 1)
+    pl.scatter(corrs_, mcorrs_)
+    def lab(l, d):
+        return '%s [%.2g +- %.2g]' %(k, np.mean(d), np.std(d))
+    pl.xlabel(lab(k, corrs_))
+    pl.ylabel(lab(mk, mcorrs_))
+    pl.grid()
+    pl.plot((-minmax,minmax), (-minmax, minmax), '--')
+    pl.subplot(1, 3, 2)
+    pl.hist(corrs_, bins=31)
+    pl.title(k)
+    pl.subplot(1, 3, 3)
+    pl.hist(mcorrs_, bins=31)
+    pl.title(mk)
 
